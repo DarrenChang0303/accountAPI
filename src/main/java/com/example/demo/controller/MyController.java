f6e0ec3fb6b38fb80ab8e6383e293306fa360a53
Darren <darrenchang0303@gmail.com>
2023-01-20 20:17:42 +0800

    fix bugs and add docker profile

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -1,18 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.User;
 import com.example.demo.service.Service;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-//import org.json.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -45,17 +36,26 @@ public class MyController {
     public Map<String,String> createAccount(@RequestBody Map<String, String> objectMap){
         Map<String,String> jsonResponse = new HashMap<>();
         if(service.accountCreate(objectMap.get("username"),objectMap.get("password"))){
-            jsonResponse.put("success","create account successful");
+            jsonResponse.put("success","create account success");
         }else {
             jsonResponse.put("false","create account fail");
         }
         return jsonResponse;
     }
 
+    /**
+     *log in
+     */
     @PostMapping("/login")
-    public boolean vertifyId(@RequestBody Map<String, String> objectMap) {
+    public Map<String,String> verifyId(@RequestBody Map<String, String> objectMap) {
+        Map<String,String> jsonResponse = new HashMap<>();
         System.out.println("Verify Id");
         System.out.println(objectMap.get("username")+objectMap.get("password"));
-        return service.accountVerify(objectMap.get("username"),objectMap.get("password"));
+        if(service.accountVerify(objectMap.get("username"),objectMap.get("password"))){
+            jsonResponse.put("success","verify id success");
+        }else {
+            jsonResponse.put("fail","verify id fail");
+        };
+        return jsonResponse;
     }
 }
--- src/main/java/com/example/demo/service/Service.java
+++ src/main/java/com/example/demo/service/Service.java
@@ -12,10 +12,6 @@ public class Service {
     Map<String, String> userMap;
     ArrayList<User> userArray = new ArrayList<User>();
 
-    public void accountSave(String userName, String password) {
-
-    }
-
     public ArrayList accountList() {
         if (userArray.size() != 0) {
             for (User user : userArray) {
@@ -29,55 +25,28 @@ public class Service {
 
     public boolean accountCreate(String username, String password) {
         boolean pass = true;
-        //Todo:refactor checkPassWord
-//        if (username.length() < 3 || username.length() > 32) {
-//            System.out.println("The number of user name characters should be between 3 to 32");
-//            return false;
-//        } else
-            if (checkPassWord(username,password)) {
+        if (checkPassWord(username, password)) {
             User user = new User(username, password);
             userArray.add(user);
-            System.out.println("Account create successfully");
-        }else {
+            System.out.println("Account create success");
+        } else {
             pass = false;
         }
-            return pass;
+        return pass;
     }
 
-    public boolean checkPassWord(String username,String password) {
-//        String pattern = "[A-Z]+[a-z]+[0-9]+";
-//        String pattern = "/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/";
-//        String pattern = ".*[0-9]{1,}.*";
-//        String pattern = ".*(.*[0-9]{1,}.*).*(.*[A-Z]{1,}.*).*(.*[a-z]{1,}.*)";
-            String patternPassword = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$";
-            String patternUserName = "^{3,32}$";//need fix
-//        Pattern pattern = Pattern.compile(patternPassword);
-//        String pattern1 = "(.*[a-z]{1,}.*)";
-//        String pattern2 = "(.*[A-Z]{1,}.*)";
-//        String pattern3 = "(.*[0-9]{1,}.*)";
+    public boolean checkPassWord(String username, String password) {
+        String patternPassword = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$";
 
-//        if (password.length() >= 8 && password.length() <= 32) {
-            if (Pattern.matches(patternPassword, password)&&3<=username.length()&&username.length()<=32) {
-                return true;
-            } else {
-//                System.out.println("password content wrong");
-                return false;
-            }
+        if (Pattern.matches(patternPassword, password) && 3 <= username.length() && username.length() <= 32) {
+            return true;
+        } else {
+            return false;
         }
-//        else {
-//            System.out.println("password length wrong");
-//            return false;
-//        }
-//        return Pattern.matches(pattern, password);
-//        return pattern1.matcher(password).matches();
-//    }
+    }
 
-//    @Override
-//    public boolean equals(Object o){
-//
-//}
     public boolean accountVerify(String username, String password) {
-        User user = new User(username,password);
+        User user = new User(username, password);
         return userArray.contains(user);
     }
 }
